1786aff092cfd7c16c1a236ff0bbceaea2d1c1d2
shahinasak98 <shahinasak98@gmail.com>
2023-03-27 19:20:31 +0530

    settings file addition

--- catalogue_project/catalogue_project/settings.py
+++ catalogue_project/catalogue_project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ah-5f-0g8h83+u$*xworyotw710wqw%gusrr65j&td-ugwl1lk'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
