51e7100fb5ee365b8ae3ac18dd1138927b38b646
shahinasak98 <shahinasak98@gmail.com>
2023-03-30 17:41:16 +0530

    II

--- catalogue_project/products/helpers.py
+++ catalogue_project/products/helpers.py
@@ -6,6 +6,7 @@ from .models import Products
 
 excel_file = os.path.join(settings.BASE_DIR, 'items.xlsx')
 
+
 def get_excel_values():
     """Helper function to add excels records to database"""
 
@@ -37,6 +38,7 @@ def get_excel_values():
             )
             product.save()
 
+
 def get_active_value(enabled):
     """ A helper function to return boolean equivalent"""
 
@@ -46,7 +48,7 @@ def get_active_value(enabled):
     else:
         active = 0
     return active
-    
+
 
 def get_products():
     """Helper function to get all the available products"""
@@ -55,20 +57,24 @@ def get_products():
     products = Products.objects.all()
     return products
 
+
 def get_topmost_parent(item_code):
     """Helper function to return topmost parent's name """
 
-    parent= None
+    parent = None
     if Products.objects.filter(item_code=item_code).exists():
         parent_product = Products.objects.get(item_code=item_code)
         if parent_product.parent_code == parent_product.item_code:
             parent = parent_product.item_name
         else:
             while parent_product.parent_code != "nan":
-                parent_product = Products.objects.get(item_code=parent_product.parent_code)
+                parent_product = Products.objects.get(
+                    item_code=parent_product.parent_code
+                )
             parent = parent_product.item_name
-        
+       
     return parent
+
 
 def get_children(item_code):
     """Helper function to print children products of given product"""
@@ -81,18 +87,22 @@ def get_children(item_code):
             data.append(child.item_code)
     return data
 
+
 def get_count_products():
     """Helper function that returns count of active and inactive products"""
 
-    data = {"active": 0, "inactive" : 0}
+    data = {"active": 0, "inactive": 0}
     data["active"] = Products.objects.filter(active=True).count()
     data["inactive"] = Products.objects.filter(active=False).count()
     return data
 
+
 def get_avg_price():
     """Helper function to get average of products assciated to (category1 and category)"""
 
-    datas = Products.objects.values('category_l1','category_l2').annotate(avg_price=Avg('price'))
+    datas = Products.objects.values(
+        'category_l1', 'category_l2'
+    ).annotate(avg_price=Avg('price'))
     return datas
 
 
--- catalogue_project/products/views.py
+++ catalogue_project/products/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Products
 from .helpers import (
-    get_products, get_topmost_parent, get_children, get_count_products, get_avg_price
+    get_products, get_topmost_parent, get_children, 
+    get_count_products, get_avg_price
 )
 # Create your views here.
+
 
 class ProductsView(generic.View):
     """View which loads all the data from database added through Excel sheet"""
 
-    def get(self,request):
+    def get(self, request):
         products = get_products()
         return render(request, 'products/index.html', {"products": products})
-    
+
+
 class ProductParentView(generic.View):
     """View for printing Parent product of a child product"""
 
@@ -20,23 +22,26 @@ class ProductParentView(generic.View):
         parent = get_topmost_parent(product_code)
         return render(request, 'products/faq2.html', {"parent": parent})
 
+
 class ProductChildrenView(generic.View):
     """View for Listing out Children of Parent Products"""
 
     def get(self, request, product_code):
         children = get_children(product_code)
-        return render(request, 'products/faq3.html', {"children": children} )
+        return render(request, 'products/faq3.html', {"children": children})
+
 
 class ProductCountView(generic.View):
     """View for count of active and inactive products"""
 
     def get(self, request):
         data = get_count_products()
-        return render(request, 'products/faq4.html', {"data": data} )
+        return render(request, 'products/faq4.html', {"data": data})
+
 
 class ProductAverageView(generic.View):
     """View for average between various types of products between category1 and category2"""
 
     def get(self, request):
         data = get_avg_price()
-        return render(request, 'products/faq5.html', {"data": data} )
+        return render(request, 'products/faq5.html', {"data": data})
